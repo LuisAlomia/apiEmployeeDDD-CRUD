a0dbc0a39d7d4608ebab035d3b23b8691cdd3010
Luis Alomia <luisfidel44@gmail.com>
2022-12-16 19:06:36 -0500

    fix: connect mongo

--- src/employee/infraestructure/express/db/mongo.ts
+++ src/employee/infraestructure/express/db/mongo.ts
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
 
-const DB_URI = `${process.env.DB_URI}`;
+const DB_URI = `${process.env.DB_URY_MONGO}`;
 
 const dbInit = async () => {
   await connect(DB_URI);
