b39d39bdb7e989fea464d4f9216f5665a89579fc
Luis Alomia <luisfidel44@gmail.com>
2022-12-16 20:56:53 -0500

    fix: fix in EmployeeRepository

--- src/employee/domain/employee.repository.ts
+++ src/employee/domain/employee.repository.ts
@@ -3,5 +3,5 @@ import { EmployeeEntity } from "./employee.entity";
 export interface EmployeeRepository {
   register(employee: EmployeeEntity): Promise<EmployeeEntity>;
   delete(employeeId: string): Promise<void>;
-  update(employee: EmployeeEntity): Promise<EmployeeEntity>;
+  update(employeeId: string, employee: EmployeeEntity): Promise<EmployeeEntity>;
 }
