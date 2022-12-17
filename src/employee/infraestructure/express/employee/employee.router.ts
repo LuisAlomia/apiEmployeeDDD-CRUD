c08dce2dec392b4f278478c7e6b548476c3f4ff1
Luis Alomia <luisfidel44@gmail.com>
2022-12-17 17:24:53 -0500

    chore: add routes getAll and getOne

--- src/employee/infraestructure/express/employee/employee.router.ts
+++ src/employee/infraestructure/express/employee/employee.router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { GetAllEmployeeUseCase } from "../../../aplication/getAllEmployee.usecase";
+import { GetOneEmployeeUseCase } from "../../../aplication/getOneEmployee.usecase";
 import { CreateEmployeeUseCase } from "../../../aplication/createEmployee.usecase";
 import { DeleteEmployeeUseCase } from "../../../aplication/deleteEmployee.usecase";
 import { UpdateEmployeeUseCase } from "../../../aplication/updateEmployee.usecase";
@@ -8,16 +10,27 @@ import { EmployeeCtrl } from "./employee.controllers";
 export const router = Router();
 
 const employeeRepository = new MongoRepository();
+const employeeGetAllUseCase = new GetAllEmployeeUseCase(employeeRepository);
+const employeeGetOneUseCase = new GetOneEmployeeUseCase(employeeRepository);
 const employeeCreateUseCase = new CreateEmployeeUseCase(employeeRepository);
 const employeeUpdateUseCase = new UpdateEmployeeUseCase(employeeRepository);
 const employeeDteleteUseCase = new DeleteEmployeeUseCase(employeeRepository);
 
 const employeeController = new EmployeeCtrl(
+  employeeGetAllUseCase,
+  employeeGetOneUseCase,
   employeeCreateUseCase,
   employeeUpdateUseCase,
   employeeDteleteUseCase
 );
 
-router.post("/", employeeController.registerEmployeeCtrl);
-router.put("/:employee_Id", employeeController.updateEmployeeCtrl);
-router.delete("/:employee_Id", employeeController.deleteEmployeeCtrl);
+router
+  .route("/")
+  .get(employeeController.getAllEmployeeCtrl)
+  .post(employeeController.registerEmployeeCtrl);
+
+router
+  .route("/:employee_Id")
+  .get(employeeController.getOneEmployeeCtrl)
+  .put(employeeController.updateEmployeeCtrl)
+  .delete(employeeController.deleteEmployeeCtrl);
