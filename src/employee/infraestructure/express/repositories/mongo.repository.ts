97f5901bf734509f02022780116e31814b503469
Luis Alomia <luisfidel44@gmail.com>
2022-12-17 17:03:16 -0500

    chore: add method getAll and getOne to MongoRepository

--- src/employee/infraestructure/express/repositories/mongo.repository.ts
+++ src/employee/infraestructure/express/repositories/mongo.repository.ts
@@ -3,6 +3,14 @@ import { EmployeeRepository } from "../../../domain/employee.repository";
 import EmployeeModel from "../models/employee.model";
 
 export class MongoRepository implements EmployeeRepository {
+  async getAll(): Promise<any> {
+    const employeeList = await EmployeeModel.find();
+    return employeeList;
+  }
+  async getOne(employeeId: string): Promise<any> {
+    const employee = await EmployeeModel.findOne({ uuid: employeeId });
+    return employee;
+  }
   async register(employee: EmployeeEntity): Promise<any> {
     const newEmployee = await EmployeeModel.create(employee);
     return newEmployee;
