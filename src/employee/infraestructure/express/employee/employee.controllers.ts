899aff9c22d0ed82b2fbf9871d03a7ad88e27f13
Luis Alomia <luisfidel44@gmail.com>
2022-12-16 21:16:00 -0500

    chore: add controllers Employee

---
+++ src/employee/infraestructure/express/employee/employee.controllers.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { CreateEmployeeUseCase } from "../../../aplication/createEmployee.usecase";
+import { DeleteEmployeeUseCase } from "../../../aplication/deleteEmployee.usecase";
+import { UpdateEmployeeUseCase } from "../../../aplication/updateEmployee.usecase";
+
+export class EmployeeCtrl {
+  private readonly _createEmployeeUseCase;
+  private readonly _updateEmployeeUseCase;
+  private readonly _deleteEmployeeUseCase;
+
+  constructor(
+    createEmployeeUseCase: CreateEmployeeUseCase,
+    updateEmployeeUseCase: UpdateEmployeeUseCase,
+    deleteEmployeeUseCase: DeleteEmployeeUseCase
+  ) {
+    this._createEmployeeUseCase = createEmployeeUseCase;
+    this._updateEmployeeUseCase = updateEmployeeUseCase;
+    this._deleteEmployeeUseCase = deleteEmployeeUseCase;
+  }
+
+  registerEmployeeCtrl = async (req: Request, resp: Response) => {
+    const { body } = req;
+
+    try {
+      const data = this._createEmployeeUseCase.run(body);
+      resp.status(201).json(data);
+    } catch (err: any) {
+      resp.status(400).json({ message: err.message });
+    }
+  };
+
+  updateEmployeeCtrl = async (req: Request, resp: Response) => {
+    const { body } = req;
+    const { employee_Id } = req.params;
+    try {
+      const data = this._updateEmployeeUseCase.run(employee_Id, body);
+      resp.status(201).json(data);
+    } catch (err: any) {
+      resp.status(400).json({ message: err.message });
+    }
+  };
+
+  deleteEmployeeCtrl = async (req: Request, resp: Response) => {
+    const { employee_Id } = req.params;
+    try {
+      const data = this._deleteEmployeeUseCase.run(employee_Id);
+      resp.status(204).json(data);
+    } catch (err: any) {
+      resp.status(400).json({ message: err.message });
+    }
+  };
+}
