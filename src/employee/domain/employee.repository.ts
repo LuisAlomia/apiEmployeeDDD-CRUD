c33cea3533f632fadda114082aa4c633efb24f88
Luis Alomia <luisfidel44@gmail.com>
2022-12-17 16:11:19 -0500

    chore: add getAll and getOne method to EmployeeRepository

--- src/employee/domain/employee.repository.ts
+++ src/employee/domain/employee.repository.ts
@@ -4,4 +4,6 @@ export interface EmployeeRepository {
   register(employee: EmployeeEntity): Promise<EmployeeEntity>;
   delete(employeeId: string): Promise<void>;
   update(employeeId: string, employee: EmployeeEntity): Promise<EmployeeEntity>;
+  getAll(): Promise<EmployeeEntity[]>;
+  getOne(employeeId: string): Promise<EmployeeEntity>;
 }
